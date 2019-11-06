015346a3bee68f42c4795bbf2b752a725c02bf45
Shmandeltoid <44483222+Shmandeltoid@users.noreply.github.com>
2019-11-06 15:12:19 -0800

    Run This Python script

--- CalibrationSetup.py
+++ CalibrationSetup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[31]:
 
 
 import numpy as np
@@ -12,17 +12,17 @@ import numpy as np
 import math
 
 
-# In[7]:
+# In[32]:
 
 
-data = pd.read_csv("~/Desktop/RadSens-Calibration/calibration_data/20191023_15_26_43.csv")
+data = pd.read_csv("calibration_data/Calibrated_Data.csv")
 
 
-# In[8]:
+# In[33]:
 
 
 #gets CPM and dose in array list
-with open("/Users/oliviakim/Desktop/RadSens-Calibration/calibration_data/20191023_15_26_43.csv", 'r') as file:
+with open("calibration_data/Calibrated_Data.csv", 'r') as file:
     lines = file.readlines()
     uSv_h = []
     CPM = []
@@ -53,19 +53,19 @@ with open("/Users/oliviakim/Desktop/RadSens-Calibration/calibration_data/2019102
                 break
         #while (lines[line][j + k] != ','): 
             #k+=1
-        uSv_h.append(temp)
+        uSv_h.append(float(temp))
         CPM.append(temp3)
     print('uSv_h:', uSv_h)
     print('CPM:', CPM)
 
 
-# In[9]:
+# In[34]:
 
 
 data
 
 
-# In[10]:
+# In[35]:
 
 
 #calculates the average of the constant factor in the equation CPM * N = DOSE
@@ -73,4 +73,91 @@ n = []
 for i in range(len(uSv_h)):
     n.append(float(uSv_h[i])/float(CPM[i]))
 print("The n of CPM * N = DOSE is", sum(n)/len(n))
+
+
+# In[36]:
+
+
+PG_data = pd.read_csv("calibration_data/data-log.txt")
+print(PG_data.columns[1])
+
+
+# In[37]:
+
+
+PG_data.loc[21] = [PG_data.columns[0],float(PG_data.columns[1]),float(PG_data.columns[2])]
+PG_data
+
+
+# In[38]:
+
+
+PG_data.columns
+PG_data.rename(columns={PG_data.columns[0]:'TimeStamp',PG_data.columns[1]:'CPM',
+                          PG_data.columns[2]:'uncertainty'}, 
+                 inplace=True)
+
+
+# In[39]:
+
+
+PG_data.columns
+PG_data
+
+
+# In[49]:
+
+
+cpm_data = np.array(PG_data.CPM)
+cpm_data
+
+
+# In[50]:
+
+
+c = np.mean(cpm_data)
+u = np.mean(uSv_h)
+u_e= np.std(uSv_h)
+print(u)
+print(u_e)
+c
+
+
+# In[69]:
+
+
+#user input collection time
+data_collection_time = 24
+c_e = np.sqrt(c * data_collection_time)/data_collection_time
+print(c_e)
+k = c_e/c
+print(k)
+
+
+# In[56]:
+
+
+PG_Calibration = u/c
+print(PG_Calibration)
+
+
+# In[58]:
+
+
+#propogating uncertainty
+calibrated_uncertainty = np.sqrt((1/c)**2 * u_e**2 + ((u)/(c)**2)**2 * c_e**2)
+calibrated_uncertainty
+
+
+# In[67]:
+
+
+percent_uncertainty = calibrated_uncertainty/PG_Calibration
+percent_uncertainty
+
+
+# In[ ]:
+
+
+
 
